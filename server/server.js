7df8e2987bf56f93e814d01c687dcbf4b7d3156a
henryhe1234 <henryhe1123@gmail.com>
2021-04-26 22:13:45 -0600

    create realtime chating using websocket

--- server/server.js
+++ server/server.js
@@ -1,4 +1,4 @@
-const { GraphQLServer,PubSub } = require("graphql-yoga");
+const { GraphQLServer, PubSub } = require("graphql-yoga");
 const messages = [];
 //! means the type is require
 const typeDefs = `
@@ -17,27 +17,39 @@ const typeDefs = `
     messages: [Message!]
   }
 `;
+
+const subscribers = [];
+const onMessagesUpdates = (fn) => subscribers.push(fn);
+
 const resolvers = {
-  Query:{
-    messages: ()=>messages,
+  Query: {
+    messages: () => messages,
   },
-  Mutation:{
-    postMessage:(parent,{user,content})=>{
+  Mutation: {
+    postMessage: (parent, { user, content }) => {
       const id = messages.length;
       messages.push({
         id,
         user,
         content,
       });
+      subscribers.forEach(fn=>fn());
       return id;
-    }
+    },
   },
-  Subscription:{
-    
-  }
-}
-
-const server = new GraphQLServer({typeDefs,resolvers});
+  Subscription: {
+    messages: {
+      subscribe: (parent, args, { pubsub }) => {
+        const channel = Math.random().toString(36).slice(2, 15);
+        onMessagesUpdates(() => pubsub.publish(channel, { messages }));
+        setTimeout(() => pubsub.publish(channel, { messages }), 0);
+        return pubsub.asyncIterator(channel);
+      },
+    },
+  },
+};
+const pubsub = new PubSub();
+const server = new GraphQLServer({ typeDefs, resolvers, context: { pubsub } });
 
 server.start(({ port }) => {
   console.log(`Server on http://localhost:${port}/`);
